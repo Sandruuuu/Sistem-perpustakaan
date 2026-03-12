8e0ebb2fa5d652423878f44059f27869f295e036
Sandruuuu <sandroindo2008@gmail.com>
2026-03-12 10:19:24 +0700

    Fix: Use PORT environment variable

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,25 @@
 import 'dotenv/config'; // Harus di baris paling atas
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { GlobalExceptionFilter } from './common/http-exception.filter';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    whitelist: true,
-    transformOptions: {
-      enableImplicitConversion  : true,
-    }
-  }));
 
-  app.useGlobalFilters(new GlobalExceptionFilter());
   const config = new DocumentBuilder()
-  .setTitle("Library API")
-  .setDescription("Backend API Sistem Perpustakaan")
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build();
-  
+    .setTitle('Sistem Perpustakaan')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0');
-  console.log(`✅ Application running on port ${port}`);
+  const host = '0.0.0.0';
+  
+  await app.listen(port, host);
+  console.log(`Application is running on port ${port}`);
 }
 bootstrap();
